c653b3f388a976b8e0dee1ea0e91fbef9b30eb44
Dennis Tobar <dennis.tobar@gmail.com>
2014-05-13 23:39:11 -0400

    Segundo commit

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php 
+<?php
 
+$f3=require('lib/base.php');
 
+$f3->route('GET /', function($f3){
+    echo $f3->get('BASE');
+});
+
+$f3->route('GET /test', function($f3){
+    echo 'test '.$f3->get('BASE');
+});
+
+$f3->run();
