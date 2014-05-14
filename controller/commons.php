c912cb437ecfcfd11b993bfad8e17edce74ed5cf
Dennis Tobar <dennis.tobar@gmail.com>
2014-05-14 14:51:51 -0400

    Primer intento de interfaz

--- controller/commons.php
+++ controller/commons.php
@@ -3,10 +3,27 @@
 namespace controller;
 
 class commons {
-    
+
+    public static function index($f3){
+        $f3->set('title', 'Commons Tools');
+        $f3->set('output', 'commons/index.html');
+        echo \Template::instance()->render('layout.html');
+    }
+
     public static function users_category($f3){
-        $db = new \helper\database($f3, 'commonswiki', 'commons');
-        var_dump($db->exec('SELECT * FROM user LIMIT 1'));
+        $cat = null;
+        if ($f3->exists('PARAMS.cat') === true){
+            $cat = $f3->get('PARAMS.cat');
+        }
+        elseif($f3->get('VERB') === 'GET'){
+            $f3->reroute('/superzerocool/commons');
+        }
+        elseif($f3->get('VERB') === 'POST'){
+            $cat = $f3->get('POST.category');
+        }
+
+        $db = new \helper\database($f3, 'commonswiki', 'commonswiki');
+        var_dump($db->exec('select img_user_text user, count(1) number from page, image, categorylinks where page_title = img_name and cl_from = page_id and cl_to = ? and page_namespace = 6 group by 1 order by 2 DESC', $cat));
     }
 
 }
--- index.php
+++ index.php
@@ -6,19 +6,8 @@ $f3=require('lib/base.php');
 $f3->config('config.ini');
 $f3->config('../fatfree.my.cnf');
 
-$f3->route('GET /superzerocool', function($f3){
-    echo $f3->get('BASE');
-});
-
-$f3->route('GET /', function($f3){
-    echo $f3->get('BASE');
-});
-
-$f3->route('GET /test', function(){
-    echo 'a';
-});
-
 $f3->route('GET /superzerocool/commons', 'controller\commons::index');
+$f3->route('POST|GET /superzerocool/commons/users', 'controller\commons::users_category');
 $f3->route('GET /superzerocool/commons/users/@cat', 'controller\commons::users_category');
 
 $f3->run();
