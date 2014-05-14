af8f654a0ab22ac39fdffe6ab11c92b8f3b9ab41
Dennis Tobar <dennis.tobar@gmail.com>
2014-05-14 15:03:40 -0400

    Fix: interpretacion de rutas en el wikimedia labs

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $f3=require('lib/base.php');
 
 $f3->config('config.ini');
 $f3->config('../fatfree.my.cnf');
+$f3->config('../local.ini');
 
 $f3->route('GET /superzerocool/commons', 'controller\commons::index');
 $f3->route('POST|GET /superzerocool/commons/users', 'controller\commons::users_category');
