42663d06a931144a2193c330014dee8985f38115
Dennis Tobar <dennis.tobar@gmail.com>
2015-07-18 03:51:27 -0300

    Finish some basic indicators, now a few steps in the GUI before sleep

--- model/stats.php
+++ model/stats.php
@@ -16,30 +16,51 @@ class stats{
     public function getTotals(){
         $data = $this->getUsers();
         $tmp = [
-            'users' => count(array_column($data, 'user_name')),
-            'uploads' => array_sum(array_column($data, 'uploads')),
-            'newbies' => array_sum(array_column($data, 'newbie')),
-            'rate' => $tmp['newbies']/$tmp['users']
-            ];
+        'users' => count(array_column($data, 'user_name')),
+        'uploads' => array_sum(array_column($data, 'uploads')),
+        'newbies' => array_sum(array_column($data, 'newbie'))
+        ];
+        $tmp['rate_newbies'] = $tmp['newbies']/($tmp['users'] ?: 1);
+        $upl_newbie = array_filter($data, function($f){ return $f['newbie'] === '1'; });
+        $tmp['newbie_uploads'] = array_sum(array_column($upl_newbie, 'uploads'));
+        $tmp['rate_newbies_uploads'] = $tmp['newbie_uploads']/($tmp['uploads'] ?: 1);
+        $tmp['percentile_25'] = self::getPercentile(25, array_column($data, 'uploads'));
+        $tmp['percentile_50'] = self::getPercentile(50, array_column($data, 'uploads'));
+        $tmp['percentile_75'] = self::getPercentile(75, array_column($data, 'uploads'));
+        $tmp['percentile_90'] = self::getPercentile(90, array_column($data, 'uploads'));
+        $tmp['percentile_99'] = self::getPercentile(99, array_column($data, 'uploads'));
         return $tmp;
-
     }
 
     public function getUsers(){
-        $db = new \helper\database(\F3::instance(), 'commonswiki', 'commonswiki');
-        $res = $db->exec("
-            select user_name, user_registration, 
-    (case when user_registration between :ini and :fin then 1 else 0 end) newbie,
-    count(1) uploads
-from categorylinks 
-join page pg on page_id = cl_from 
-join image img on img_name = page_title 
-join `user` us on img_user = user_id
-where cl_to = :cat
-and cl_type = 'file'
-group by 1, 2, 3
-order by 4 desc", $this->params);
-        return $res;
+        if(isset($this->list) === false){
+            $db = new \helper\database(\F3::instance(), 'commonswiki', 'commonswiki');
+            $res = $db->exec("
+                select user_name, user_registration, 
+                (case when user_registration between :ini and :fin then 1 else 0 end) newbie,
+                count(1) uploads
+                from categorylinks 
+                join page pg on page_id = cl_from 
+                join image img on img_name = page_title 
+                join `user` us on img_user = user_id
+                where cl_to = :cat
+                and cl_type = 'file'
+                group by 1, 2, 3
+                order by 4 desc", $this->params);
+            $this->list = $res;
+        }
+        return $this->list;
+    }
+
+    /** http://stackoverflow.com/a/24049361 **/
+    public function getPercentile($percentile, $array){
+        sort($array);
+        $index = ($percentile/100) * count($array);
+        $idx = ceil($index);
+        return [
+            'sum' => array_sum(array_slice($array, 0, $idx > count($array) ? $idx -1 : $idx)),
+            'count' => count(array_slice($array, 0, $idx > count($array) ? $idx -1 : $idx))
+            ];
     }
 
 
