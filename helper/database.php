b63269b435e9fe1cd9b6a265bbce661df591d382
superzerocool <dennis.tobar@gmail.com>
2017-06-01 12:35:00 -0400

    Composer way

--- helper/database.php
+++ helper/database.php
@@ -3,10 +3,10 @@
 namespace helper;
 
 class database extends \DB\SQL{
-    
+
     public function __construct($f3, $server = 'commonswiki', $database = 'commonswiki'){
         $dns = sprintf('mysql:host=%s.labsdb;dbname=%s_p', $server, $database);
-        parent::__construct($dns, $f3->get('user'), $f3->get('password'));
+        parent::__construct($dns, $f3->get('db.user'), $f3->get('db.password'));
     }
 
 }
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-$f3 = require 'lib/base.php';
+require_once "vendor/autoload.php";
+
+$f3 = \Base::instance();
 
 $f3->config('config.ini');
 $f3->config('../fatfree.my.cnf');
