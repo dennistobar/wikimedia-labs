68e2893542f543b9831806b4111e46797dbb59c8
superzerocool <dennis.tobar@gmail.com>
2018-01-25 23:43:25 -0300

    Mejorando algoritmo de búsqueda de imágenes

--- app/model/commons/search.php
+++ app/model/commons/search.php
@@ -13,26 +13,12 @@ class search
             return in_array($f, ['cat', 'user']);
         }, ARRAY_FILTER_USE_KEY);
 
-        $qDetails = '
-            select ifnull(oi_user_text, img_user_text) as user, img_name, img_size, img_metadata, img_timestamp, img_width, img_height,
-                (select count(distinct oi_timestamp) from oldimage where oi_name = img_name) as revs
-            from page, image
-            left join oldimage o1
-                on oi_name = img_name
-                and oi_timestamp = (select min(o2.oi_timestamp) from oldimage o2 where o1.oi_name = o2.oi_name)
-            , categorylinks
-            where page_title = img_name
-                and cl_from = page_id
-                and cl_to = :cat
-                and page_namespace = 6
-                and ifnull(oi_user_text , img_user_text) = ifnull(:user, ifnull(oi_user_text , img_user_text))
-                order by img_timestamp DESC;';
-        return \model\database::instance('commonswiki', 'commonswiki')->exec($qDetails, $options, 50);
+        return searchBase::select([], $options);
     }
 
     public static function resume($options = [])
     {
-        $result = self::search($options);
+        $result = searchBase::select(['user', 'img_name'], $options);
         $users = array_combine(array_column($result, 'user'), array_fill(0, count($result), 0));
         array_walk($users, function (&$el, $index) use ($result) {
             $el = count(array_intersect(array_column($result, 'user'), [$index]));
---
+++ app/model/commons/searchBase.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace model\commons;
+
+class searchBase
+{
+    private static $data = [
+        'user' => 'ifnull(oi_user_text, img_user_text)',
+        'revs' => '(select count(distinct oi_timestamp) from oldimage where oi_name = img_name)',
+        'img_name', 'img_size', 'img_timestamp', 'img_width', 'img_height',
+        'user_registration'
+    ];
+
+    public static function select($params = [], $options = [])
+    {
+        $select = [];
+        $params = $params ?: self::$data;
+        foreach ($params as $param) {
+            if (array_key_exists($param, self::$data)) {
+                $select[] = self::$data[$param].' as '.$param;
+            } else {
+                $select[] = $param;
+            }
+        }
+
+        $qDetails = '
+            select '.implode(',', $select).'
+            from page, image
+            left join oldimage o1
+                on oi_name = img_name
+                and oi_timestamp = (select min(o2.oi_timestamp) from oldimage o2 where o1.oi_name = o2.oi_name)
+            left join `user`
+                on user_id = ifnull(oi_user, img_user)
+            , categorylinks
+            where page_title = img_name
+                and cl_from = page_id
+                and cl_to = :cat
+                and page_namespace = 6
+                and ifnull(oi_user_text , img_user_text) = ifnull(:user, ifnull(oi_user_text , img_user_text))
+                order by img_timestamp DESC';
+        return \model\database::instance('commonswiki', 'commonswiki')->exec($qDetails, $options, 50);
+    }
+}
