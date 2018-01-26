e2e240d0ad8efed80259ab20f129acc9c7b91d7f
superzerocool <dennis.tobar@gmail.com>
2018-01-25 23:51:39 -0300

    Ajustes en la búsqueda

--- app/model/commons/search.php
+++ app/model/commons/search.php
@@ -18,6 +18,13 @@ class search
 
     public static function resume($options = [])
     {
+        $options = array_merge(['cat' => null, 'user' => null], $options);
+        $options['cat'] = str_replace(' ', '_', $options['cat']);
+
+        $options = array_filter($options, function ($f) {
+            return in_array($f, ['cat', 'user']);
+        }, ARRAY_FILTER_USE_KEY);
+        
         $result = searchBase::select(['user', 'img_name'], $options);
         $users = array_combine(array_column($result, 'user'), array_fill(0, count($result), 0));
         array_walk($users, function (&$el, $index) use ($result) {
